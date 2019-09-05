7a5b1aedd068e627e1f0b2c68f624fd49d7caf50
jonniebigodes <jonniebigodes@hotmail.com>
2019-09-05 01:36:25 +0100

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
 import React from "react"
-
-export default () => <div>Hello world!</div>
+import Loadable from "react-loadable"
+import "../assets/css/menu.css"
+// component to show something to the user while the actual component is loaded on the client side
+const loader = () => <div>Loading Menu...</div>
+//
+const MyMenuLoadable = Loadable({
+  loader: () => import("../components/Menu.js"),
+  loading: loader,
+})
+export default () => (
+  <div className="App">
+    <MyMenuLoadable />
+  </div>
+)
